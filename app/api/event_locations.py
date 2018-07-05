419fb4fd22147d92b67d37de7d3cc2c78a01ae8d
Rainb <elektrischrainbow@gmail.com>
2018-07-05 23:19:14 +0000

    Comment stuff

--- app/api/event_locations.py
+++ app/api/event_locations.py
@@ -19,40 +19,40 @@ class EventLocationList(ResourceList):
                   'model': EventLocation}
 
 
-class EventLocationDetail(ResourceDetail):
-    """
-    Event location detail by id
-    """
-    def before_get_object(self, view_kwargs):
-        """
-        before get method to get the resource id for fetching details
-        :param view_kwargs:
-        :return:
-        """
-        if view_kwargs.get('event_identifier'):
-            event = safe_query(self, Event, 'identifier', view_kwargs['event_identifier'], 'event_identifier')
-            view_kwargs['event_id'] = event.id
+# class EventLocationDetail(ResourceDetail):
+#     """
+#     Event location detail by id
+#     """
+#     def before_get_object(self, view_kwargs):
+#         """
+#         before get method to get the resource id for fetching details
+#         :param view_kwargs:
+#         :return:
+#         """
+#         if view_kwargs.get('event_identifier'):
+#             event = safe_query(self, Event, 'identifier', view_kwargs['event_identifier'], 'event_identifier')
+#             view_kwargs['event_id'] = event.id
 
-        if view_kwargs.get('event_id'):
-            event = safe_query(self, Event, 'id', view_kwargs['event_id'], 'event_id')
-            if event.event_type_id:
-                view_kwargs['id'] = event.event_location_id
-            else:
-                view_kwargs['id'] = None
+#         if view_kwargs.get('event_id'):
+#             event = safe_query(self, Event, 'id', view_kwargs['event_id'], 'event_id')
+#             if event.event_type_id:
+#                 view_kwargs['id'] = event.event_location_id
+#             else:
+#                 view_kwargs['id'] = None
 
-    schema = EventLocationSchema
-    data_layer = {'session': db.session,
-                  'model': EventLocation,
-                  'methods': {
-                      'before_get_object': before_get_object
-                  }}
+#     schema = EventLocationSchema
+#     data_layer = {'session': db.session,
+#                   'model': EventLocation,
+#                   'methods': {
+#                       'before_get_object': before_get_object
+#                   }}
 
 
-class EventLocationRelationship(ResourceRelationship):
-    """
-    Event location Relationship
-    """
-    decorators = (api.has_permission('is_admin', methods="PATCH,DELETE"),)
-    schema = EventLocationSchema
-    data_layer = {'session': db.session,
-                  'model': EventLocation}
+# class EventLocationRelationship(ResourceRelationship):
+#     """
+#     Event location Relationship
+#     """
+#     decorators = (api.has_permission('is_admin', methods="PATCH,DELETE"),)
+#     schema = EventLocationSchema
+#     data_layer = {'session': db.session,
+#                   'model': EventLocation}
